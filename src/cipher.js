fa6c08752272cad5fa0e0adbcb716a71bf0f8109
atzina <atzina@gmail.com>
2022-10-05 18:08:16 -0500

    primer commit consciente

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,55 @@
+
 const cipher = {
-  // ...
+
+encode:  function(offset, string) {
+
+
+  let cifradaConClave = " ";//resultado
+  let palabraConClave = " ";
+
+
+    for(var i=0; i<string.length; i++){
+
+      let palabraAscci = string.charCodeAt(i);
+      console.log(palabraAscci);
+
+      if (palabraAscci>=65 && palabraAscci<=90){
+      palabraConClave= ((palabraAscci - 65 + parseInt(offset)) % 26) + 65;
+      console.log(palabraConClave)
+        //cifradaConClave=string.charCodeAt(i) - 65 +parseInt(offset))%26)+65;
+
+      }else if(palabraAscci === 32){
+        cifradaConClave - 32;
+
+      }
+
+      cifradaConClave += String.fromCharCode(palabraConClave);
+          console.log(cifradaConClave);
+      //cifradaConClave = String.fromCharCode(palabraConClave);
+      //let cifradaConClave= String.fromCharCode(((palabraAscci-65+offset)%26)+65);
+      //document.getElementById('spancifrado').innerHTML=cifradaConClave;
+    }
+return cifradaConClave;
+
+
+
+},
+decode: function(offfsetUno, stringUno) {
+
+  let decifradaConClave = " ";
+
+  for(var i=0; i<stringUno.length; i++){
+    let acertijoAscci= stringUno.charCodeAt(i);
+    let acertijoConClave = ((acertijoAscci + 65 - parseInt(offfsetUno))%26) + 65;
+    decifradaConClave += String.fromCharCode(acertijoConClave);
+  }
+  return decifradaConClave;
+
+
+
+
+}
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let resultado = document.getElementById("spancifrado");
+
+document.getElementById("botonDos").addEventListener("click", cifrar);
+
+function cifrar(){
+  const string = document.getElementById("string").value;
+  const offset = document.getElementById("offset").value;
+
+  resultado.innerHTML= cipher.encode(offset, string);
+
+}
+
+
+let resultadoUno = document.getElementById("spandecifrado");
+
+document.getElementById("botonUno").addEventListener("click", decifrar);
+
+function decifrar(){
+  const stringUno = document.getElementById("stringUno").value;
+  const offfsetUno = document.getElementById("offfsetUno").value;
+
+  resultadoUno.innerHTML = cipher.decode(offfsetUno, stringUno);
+}
